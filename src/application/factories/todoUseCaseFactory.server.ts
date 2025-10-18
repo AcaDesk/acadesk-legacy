7436346de8112ea743f23a1fb4e80b4061998e70
Jong MIn <unib335@naver.com>
2025-10-19 01:19:07 +0900

    [Feat]: Todo 유스케이스 팩토리 추가 - 클라이언트 및 서버 사이드에서의 Todo 관련 유스케이스 생성 기능 구현

--- src/application/factories/todoUseCaseFactory.server.ts
+++ src/application/factories/todoUseCaseFactory.server.ts
@@ -1,9 +1,9 @@
 /**
- * Todo Use Case Factory (Client-side)
- * TODO 유스케이스 팩토리 - 클라이언트 컴포넌트용
+ * Todo Use Case Factory (Server-side)
+ * TODO 유스케이스 팩토리 - Server Actions 및 서버 컴포넌트용
  */
 
-import { createClient } from '@/lib/supabase/client'
+import { createClient } from '@/lib/supabase/server'
 import { SupabaseTodoRepository } from '@/infrastructure/database/SupabaseTodoRepository'
 import { CreateTodoUseCase } from '../use-cases/todo/CreateTodoUseCase'
 import { CreateTodosForStudentsUseCase } from '../use-cases/todo/CreateTodosForStudentsUseCase'
@@ -17,89 +17,89 @@ import { GetTodoUseCase } from '../use-cases/todo/GetTodoUseCase'
 import { GetTodosUseCase } from '../use-cases/todo/GetTodosUseCase'
 
 /**
- * TODO 리포지토리 생성 (클라이언트 사이드)
+ * TODO 리포지토리 생성 (서버 사이드)
  */
-function createTodoRepository() {
-  const supabase = createClient()
+async function createTodoRepository() {
+  const supabase = await createClient()
   return new SupabaseTodoRepository(supabase)
 }
 
 /**
  * CreateTodo 유스케이스 생성
  */
-export function createCreateTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createCreateTodoUseCase() {
+  const repository = await createTodoRepository()
   return new CreateTodoUseCase(repository)
 }
 
 /**
  * UpdateTodo 유스케이스 생성
  */
-export function createUpdateTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createUpdateTodoUseCase() {
+  const repository = await createTodoRepository()
   return new UpdateTodoUseCase(repository)
 }
 
 /**
  * CompleteTodo 유스케이스 생성
  */
-export function createCompleteTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createCompleteTodoUseCase() {
+  const repository = await createTodoRepository()
   return new CompleteTodoUseCase(repository)
 }
 
 /**
  * VerifyTodo 유스케이스 생성
  */
-export function createVerifyTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createVerifyTodoUseCase() {
+  const repository = await createTodoRepository()
   return new VerifyTodoUseCase(repository)
 }
 
 /**
  * DeleteTodo 유스케이스 생성
  */
-export function createDeleteTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createDeleteTodoUseCase() {
+  const repository = await createTodoRepository()
   return new DeleteTodoUseCase(repository)
 }
 
 /**
  * GetTodo 유스케이스 생성
  */
-export function createGetTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createGetTodoUseCase() {
+  const repository = await createTodoRepository()
   return new GetTodoUseCase(repository)
 }
 
 /**
  * CreateTodosForStudents 유스케이스 생성 (Bulk)
  */
-export function createCreateTodosForStudentsUseCase() {
-  const repository = createTodoRepository()
+export async function createCreateTodosForStudentsUseCase() {
+  const repository = await createTodoRepository()
   return new CreateTodosForStudentsUseCase(repository)
 }
 
 /**
  * VerifyTodos 유스케이스 생성 (Bulk)
  */
-export function createVerifyTodosUseCase() {
-  const repository = createTodoRepository()
+export async function createVerifyTodosUseCase() {
+  const repository = await createTodoRepository()
   return new VerifyTodosUseCase(repository)
 }
 
 /**
  * RejectTodo 유스케이스 생성
  */
-export function createRejectTodoUseCase() {
-  const repository = createTodoRepository()
+export async function createRejectTodoUseCase() {
+  const repository = await createTodoRepository()
   return new RejectTodoUseCase(repository)
 }
 
 /**
  * GetTodos 유스케이스 생성 (다수 조회, 필터 가능)
  */
-export function createGetTodosUseCase() {
-  const repository = createTodoRepository()
+export async function createGetTodosUseCase() {
+  const repository = await createTodoRepository()
   return new GetTodosUseCase(repository)
 }
