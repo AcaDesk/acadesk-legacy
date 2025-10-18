295f1d2ac31fd7c2707dd4dc826fc3f9374b367e
Jong MIn <unib335@naver.com>
2025-10-19 01:19:07 +0900

    [Feat]: Todo 유스케이스 팩토리 추가 - 클라이언트 및 서버 사이드에서의 Todo 관련 유스케이스 생성 기능 구현

---
+++ src/application/factories/todoTemplateUseCaseFactory.client.ts
@@ -0,0 +1,51 @@
+/**
+ * TodoTemplate Use Case Factory (Client-side)
+ * TODO 템플릿 유스케이스 팩토리 - 클라이언트 컴포넌트용
+ */
+
+import { createClient } from '@/lib/supabase/client'
+import { SupabaseTodoTemplateRepository } from '@/infrastructure/database/SupabaseTodoTemplateRepository'
+import { GetTodoTemplatesUseCase } from '../use-cases/todo-template/GetTodoTemplatesUseCase'
+import { CreateTodoTemplateUseCase } from '../use-cases/todo-template/CreateTodoTemplateUseCase'
+import { UpdateTodoTemplateUseCase } from '../use-cases/todo-template/UpdateTodoTemplateUseCase'
+import { DeleteTodoTemplateUseCase } from '../use-cases/todo-template/DeleteTodoTemplateUseCase'
+
+/**
+ * TodoTemplate 리포지토리 생성 (클라이언트 사이드)
+ */
+function createTodoTemplateRepository() {
+  const supabase = createClient()
+  return new SupabaseTodoTemplateRepository(supabase)
+}
+
+/**
+ * GetTodoTemplates 유스케이스 생성
+ */
+export function createGetTodoTemplatesUseCase() {
+  const repository = createTodoTemplateRepository()
+  return new GetTodoTemplatesUseCase(repository)
+}
+
+/**
+ * CreateTodoTemplate 유스케이스 생성
+ */
+export function createCreateTodoTemplateUseCase() {
+  const repository = createTodoTemplateRepository()
+  return new CreateTodoTemplateUseCase(repository)
+}
+
+/**
+ * UpdateTodoTemplate 유스케이스 생성
+ */
+export function createUpdateTodoTemplateUseCase() {
+  const repository = createTodoTemplateRepository()
+  return new UpdateTodoTemplateUseCase(repository)
+}
+
+/**
+ * DeleteTodoTemplate 유스케이스 생성
+ */
+export function createDeleteTodoTemplateUseCase() {
+  const repository = createTodoTemplateRepository()
+  return new DeleteTodoTemplateUseCase(repository)
+}
